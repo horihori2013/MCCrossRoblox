e8fc03a6833f46b71cd86b027314eff1d361d8e8
horihori2013 <heoriho@gmail.com>
2025-10-18 20:37:32 +0900

    First Step - Generated

---
+++ src/main/kotlin/com/github/horihori2013/mccrossroblox.kt
@@ -0,0 +1,14 @@
+package com.github.horihori2013
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class mccrossroblox : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
